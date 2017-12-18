1ef7c03eeb38d96f8bdfaba5aa62349e893a5fd0
michael bychkowski <mbychkowski@gmail.com>
2017-12-17 20:16:57 -0500

    basic outline for implimentation

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+    $('#start').on('click', beginGame)
---
+++ assets/js/questions.js
@@ -0,0 +1,16 @@
+var question = [
+  'How are you feeling today?',
+  'How were you feeling yesterday',
+  'What time is it'
+];
+
+var correctChoice = [
+  'good',
+  'not as good as today',
+  'noon'];
+
+var wrongChoice = [
+  ['bad', 'okay', 'angry'],
+  ['super', 'splendid', 'excellent'],
+  ['13:00', '11:59', '12:01']
+];
